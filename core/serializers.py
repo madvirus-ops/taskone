ff8b20fa9feffad4125ac6aa2c84d60ebd42cf25
madvirus-ops <edwinayabie1@gmail.com>
2022-11-04 11:37:04 +0100

    fix: back to cbv

--- core/serializers.py
+++ core/serializers.py
@@ -1,13 +1,10 @@
-"""IF YOU'RE READING THIS YOUR ARE IN THE WRONG FILE, 
-I DID NOT USE SERIALIZERS FOR ANYTHING AGAIN
- AND I DO NOT WANT TO DELETE IT BECUASE OF FUTURE REFFERENCE"""
 
-# from rest_framework import serializers
-# from .models import ArithmeticModel
+from rest_framework import serializers
+from .models import ArithmeticModel
 
 
-# class ArithmeticSerializer(serializers.ModelSerializer):
+class ArithmeticSerializer(serializers.ModelSerializer):
 
-#     class Meta:
-#         model = ArithmeticModel
-#         fields = ('x','y','operation_type',)
+    class Meta:
+        model = ArithmeticModel
+        fields = ('x','y','operation_type',)
